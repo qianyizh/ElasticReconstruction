d7e053aff5a58f9496a12f8ef82584571b06e8f7
qianyizh <qianyizh@stanford.edu>
2013-11-07 11:33:21 -0800

    Commit "external" dependencies for FragmentOptimizer

---
+++ FragmentOptimizer/external/SuiteSparse/Bin/CHOLMOD_test.cmd
@@ -0,0 +1,8 @@
+cls
+cholmod_demo < Matrix/bcsstk01.tri
+cholmod_demo < Matrix/lp_afiro.tri
+cholmod_demo < Matrix/can___24.mtx
+cholmod_demo < Matrix/c.tri
+cholmod_simple < Matrix/c.tri
+cholmod_simple < Matrix/can___24.mtx
+cholmod_simple < Matrix/bcsstk01.tri
